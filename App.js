db1b7ce1879cd35cb9c51a462653bbd7d5d7648a
SUJIN S <sujinjuly4@gmail.com>
2021-06-16 21:38:56 +0530

    change

--- App.js
+++ App.js
@@ -15,7 +15,6 @@ const App = () => {
   return (
     <Provider store={store}>
       <I18n translations={translations} initialLang="en">
-
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
           <Route />
         </SafeAreaView>
